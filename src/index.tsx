802da2088b811e6e9131f772068ff498740c4660
Mauricio Rivera <mauriver21@gmail.com>
2022-01-15 11:48:29 -0500

    update: installed bootstrap 5 and configured prettierrc

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
