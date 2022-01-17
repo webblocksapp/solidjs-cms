a01880e879dee7081b833233022b1f0df663f979
Mauricio Rivera <mauriver21@gmail.com>
2022-01-16 21:50:45 -0500

    refactor: added yarn as npm manager

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -1,27 +1,8 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import { Box } from "@shared/components";
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return <Box>Hola mundo</Box>;
 };
 
 export default App;
