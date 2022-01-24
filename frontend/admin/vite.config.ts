0200f95f5c4f6f4d6b7955a6c906a0047455274c
Mauricio Rivera <c.rivera@globant.com>
2022-01-24 15:43:59 -0500

    update: updated some base project configs.

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -3,7 +3,10 @@ import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [solidPlugin(), tsconfigPaths()],
+  plugins: [
+    solidPlugin(),
+    tsconfigPaths({ projects: ['./tsconfig.json', '../shared/tsconfig.json'] }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
