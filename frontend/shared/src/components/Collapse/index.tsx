3beca3702b2af6e7b2dd761defa538830f6eab8c
Mauricio Rivera <c.rivera@globant.com>
2022-01-31 15:24:25 -0500

    update: added collapse handler prop.

--- frontend/shared/src/components/Collapse/index.tsx
+++ frontend/shared/src/components/Collapse/index.tsx
@@ -1,26 +1,40 @@
 import { DivElement } from '@app-types';
 import * as bootstrap from 'bootstrap';
-import { Component, createEffect, createSignal, onMount } from 'solid-js';
+import { Component, createEffect, createSignal, mergeProps, onMount } from 'solid-js';
 
+export type CollapseHandler = { toggle: () => void };
 export interface CollapseProps extends DivElement {
-  in?: boolean;
+  expanded?: boolean;
+  setHandler: (handler: CollapseHandler) => CollapseHandler;
 }
 
 export const Collapse: Component<CollapseProps> = (props) => {
+  const [expand, setExpand] = createSignal<boolean>();
+  props = mergeProps({ expanded: true }, props);
+
   let div: HTMLDivElement | undefined;
   let collapsableArea: bootstrap.Collapse | undefined;
 
-  const toggle = (flag?: boolean) => {
+  const _toggle = (flag?: boolean) => {
     flag && collapsableArea?.show();
     flag === false && collapsableArea?.hide();
   };
 
+  const toggle = () => {
+    if (!div?.classList.contains('collapsing')) {
+      setExpand(!expand());
+    }
+  };
+
   onMount(() => {
     collapsableArea = new bootstrap.Collapse(div as any, { toggle: false });
-    toggle(props?.in);
+    setExpand(props?.expanded);
+    props.setHandler({ toggle });
   });
 
-  createEffect(() => toggle(props?.in));
+  createEffect(() => _toggle(expand()));
 
-  return <div {...props} classList={{ collapse: props.in === false }} ref={div} />;
+  return (
+    <div {...props} classList={{ collapse: props.expanded === false, 'collapse show': props.expanded }} ref={div} />
+  );
 };
--- frontend/shared/src/layouts/MainLayout/index.tsx
+++ frontend/shared/src/layouts/MainLayout/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Link, Navbar, Collapse, Button } from '@components';
+import { Box, Link, Navbar, Collapse, Button, CollapseHandler } from '@components';
 import { Component, createSignal } from 'solid-js';
 
 export const MainLayout: Component = () => {
-  const [expand, setExpand] = createSignal<boolean>(false);
+  const [collapseHandler, setCollapseHandler] = createSignal<CollapseHandler>();
 
   return (
     <Box fullHeight display="grid" gridTemplateRows="auto 1fr auto">
@@ -10,8 +10,18 @@ export const MainLayout: Component = () => {
         <Link href="/">Home</Link>
       </Navbar>
       <Box>
-        <Button onClick={() => setExpand(!expand())}>Expand</Button>
-        <Collapse in={expand()}></Collapse>
+        <Button onClick={() => collapseHandler()?.toggle()}>Expand</Button>
+        <Collapse expanded={true} setHandler={setCollapseHandler}>
+          <ul>
+            <li>Hello world</li>
+            <li>Hello world</li>
+            <li>Hello world</li>
+            <li>Hello world</li>
+            <li>Hello world</li>
+            <li>Hello world</li>
+            <li>Hello world</li>
+          </ul>
+        </Collapse>
       </Box>
       <Box>Footer</Box>
     </Box>
