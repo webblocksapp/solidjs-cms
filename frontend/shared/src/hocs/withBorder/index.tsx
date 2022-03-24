64c7ea7300f132e70bbf7393164c054e813ab2de
Mauricio Rivera <mauriver21@gmail.com>
2022-03-24 09:13:18 -0500

    refactor: refactored withBorder with classList

--- frontend/shared/src/hocs/withBorder/index.tsx
+++ frontend/shared/src/hocs/withBorder/index.tsx
@@ -1,59 +1,30 @@
-import { Component, createSignal, createEffect, mergeProps } from 'solid-js';
+import { Component, mergeProps } from 'solid-js';
 import { BorderProps } from '@app-types';
 
 export const withBorder = <T extends { class?: string }>(BaseComponent: Component<T>) => {
   return (props: T & BorderProps) => {
-    const [borderWidthClass, setBorderScaleClass] = createSignal<string>('');
-    const [borderClass, setBorderClass] = createSignal<string>('');
-    const [borderTopClass, setBorderTopClass] = createSignal<string>('');
-    const [borderRightClass, setBorderRightClass] = createSignal<string>('');
-    const [borderBottomClass, setBorderBottomClass] = createSignal<string>('');
-    const [borderLeftClass, setBorderLeftClass] = createSignal<string>('');
     props = mergeProps({ class: '' }, props);
-
-    const computeBorderScaleClass = (width?: BorderProps['border']) => {
-      setBorderScaleClass(() => (width ? ` border-${width} ` : ''));
-    };
-
-    const computeBorderClass = (width?: BorderProps['border']) => {
-      computeBorderScaleClass(width);
-      setBorderClass(() => (width ? ` border ` : ''));
-    };
-
-    const computeBorderTopClass = (flag?: BorderProps['borderTop']) => {
-      setBorderTopClass(() => (props.borderTop ? ` border-top-${flag} ` : ''));
-    };
-
-    const computeBorderRightClass = (flag?: BorderProps['borderRight']) => {
-      setBorderRightClass(() => (props.borderRight ? ` border-end-${flag} ` : ''));
-    };
-
-    const computeBorderBottomClass = (flag?: BorderProps['borderBottom']) => {
-      setBorderBottomClass(() => (props.borderBottom ? ` border-bottom-${flag} ` : ''));
-    };
-
-    const computeBorderLeftClass = (flag?: BorderProps['borderLeft']) => {
-      setBorderLeftClass(() => (props.borderLeft ? ` border-start-${flag} ` : ''));
-    };
-
-    createEffect(() => computeBorderClass(props.border));
-    createEffect(() => computeBorderTopClass(props.borderTop));
-    createEffect(() => computeBorderRightClass(props.borderRight));
-    createEffect(() => computeBorderBottomClass(props.borderBottom));
-    createEffect(() => computeBorderLeftClass(props.borderLeft));
 
     return (
       <BaseComponent
         {...props}
-        class={
-          props.class +
-          borderWidthClass() +
-          borderClass() +
-          borderTopClass() +
-          borderRightClass() +
-          borderBottomClass() +
-          borderLeftClass()
-        }
+        class={props.class}
+        classList={{
+          border: props.border,
+          'border-1': props.border == 1,
+          'border-2': props.border == 2,
+          'border-3': props.border == 3,
+          'border-4': props.border == 4,
+          'border-5': props.border == 5,
+          'border-top': props.borderTop,
+          'border-top-0': props.borderTop == 0,
+          'border-end': props.borderRight,
+          'border-right-0': props.borderRight == 0,
+          'border-bottom': props.borderBottom,
+          'border-bottom-0': props.borderBottom == 0,
+          'border-start': props.borderLeft,
+          'border-start-0': props.borderLeft == 0,
+        }}
       />
     );
   };
